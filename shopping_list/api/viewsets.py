b4ebc9bcfe68d53a5fdbb6ca667438633ed80d9e
Iago Virgílio <iagovirgilio@gmail.com>
2024-10-11 10:36:52 -0300

    Testar rotas da api

--- shopping_list/api/viewsets.py
+++ shopping_list/api/viewsets.py
@@ -1,3 +1,4 @@
+# from rest_framework.renderers import JSONRenderer
 from rest_framework.viewsets import ModelViewSet
 
 from shopping_list.api.serializers import ShoppingItemSerializer
@@ -7,3 +8,4 @@ from shopping_list.models import ShoppingItem
 class ShoppingItemViewSet(ModelViewSet):
     queryset = ShoppingItem.objects.all()
     serializer_class = ShoppingItemSerializer
+    # renderer_classes = [JSONRenderer]
