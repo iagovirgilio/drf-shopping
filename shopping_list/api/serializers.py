4d5a764b0f05907969471613887fca737976ce7a
Iago Virgílio <iagovirgilio@gmail.com>
2024-10-11 13:04:08 -0300

    ADD ShoppingItemSerializer

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 
-from shopping_list.models import ShoppingItem
+from shopping_list.models import ShoppingItem, ShoppingList
 
 
 class ShoppingItemSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = ShoppingItem
         fields = ["id", "name", "purchased"]
+
         read_only_fields = ('id',)
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    shopping_items = ShoppingItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ShoppingList
+        fields = ['id', 'name', 'shopping_items']
