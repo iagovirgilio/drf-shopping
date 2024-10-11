0621d6409922e1236940d6a64dfa813056645430
Iago Virgílio <iagovirgilio@gmail.com>
2024-10-11 13:25:21 -0300

    Multiple shopping lists

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
+from shopping_list.api.views import AddShoppingItem, ShoppingItemDetail, ListAddShoppingList, ShoppingListDetail
 
-from shopping_list.api.viewsets import ShoppingItemViewSet
+# from shopping_list.api.viewsets import ShoppingItemViewSet
 
 
-router = routers.DefaultRouter()
-router.register("shopping-items", ShoppingItemViewSet, basename="shopping-items")
+# router = routers.DefaultRouter()
+# router.register("shopping-items", ShoppingItemViewSet, basename="shopping-items")
 
 urlpatterns = [
-    path("api/", include(router.urls)),
+    # path("api/", include(router.urls)),
+    path("api/shopping-lists/", ListAddShoppingList.as_view(), name="all-shopping-lists"),
+    path("api/shopping-lists/<uuid:pk>/", ShoppingListDetail.as_view(), name="shopping-list-detail"),
+    path("api/shopping-lists/<uuid:pk>/shopping-items/", AddShoppingItem.as_view(), name="add-shopping-item"),
+    path("api/shopping-lists/<uuid:pk>/shopping-items/<uuid:item_pk>/", ShoppingItemDetail.as_view(), name="shopping-item-detail"),
 ]
